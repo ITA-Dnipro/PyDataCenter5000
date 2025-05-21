969bb846c4fcb84995f0ad97a3c917dd8041f0bf
Maria <marusjakarassmaria789@gmail.com>
2025-05-21 19:26:40 +0300

    Create the ServerStatus model

--- pydata_center/monitoring/models.py
+++ pydata_center/monitoring/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ServerStatus(models.Model):
+    hostname = models.Charfield(max_length=100)
+    ip = models.GenericIPAddressField()
+    uptime = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.hostname} - {self.timestamp}"

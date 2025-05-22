25b0c4609e38c9973fefb37bafe804a774db97f8
YelyzavetaV <velizhaninae@gmail.com>
2025-05-22 12:33:36 +0200

    feat: add attribute smtp_processes to SMTPAgent

--- agents/smtp.py
+++ agents/smtp.py
@@ -4,8 +4,10 @@ from .agent import ServerAgent
 
 class SMTPAgent(ServerAgent):
 
-    def __init__(self):
+    def __init__(self, smtp_processes=None):
         super(SMTPAgent, self).__init__()
+
+        self.processes = smtp_processes or ["postfix", "exim", "sendmail"]
 
         self.port = 25
 
@@ -18,10 +20,7 @@ class SMTPAgent(ServerAgent):
 
             return (
                 self.port_open()
-                and any(
-                    process in output
-                    for process in ["postfix", "exim", "sendmail"]
-                )
+                and any(proc in output for proc in self.processes)
             )
         except Exception:
             return False

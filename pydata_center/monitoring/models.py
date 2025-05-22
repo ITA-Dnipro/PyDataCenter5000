fce948e69a344853f469cf35aebb06afc3f9617e
Maria <marusjakarassmaria789@gmail.com>
2025-05-22 14:42:49 +0300

    change request

--- pydata_center/monitoring/models.py
+++ pydata_center/monitoring/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 class ServerStatus(models.Model):
+    class Meta:
+        indexes = [
+        models.Index(fields=['hostname', 'timestamp']),
+    ]
+        
     hostname = models.CharField(max_length=100)
     ip = models.GenericIPAddressField()
     uptime = models.CharField(max_length=100)
     timestamp = models.DateTimeField(auto_now_add=True)
+    os = models.CharField(max_length=50)
+    healthy = models.BooleanField(default=False)
+    service = models.CharField(max_length=50)
+    last_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.hostname} - {self.timestamp}"

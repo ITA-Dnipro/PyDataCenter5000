180b7541b88970661ce540cdf04707b922896208
Maria <marusjakarassmaria789@gmail.com>
2025-05-23 18:10:24 +0300

    Change uptime to FloatField and renamed service to servername

--- pydata_center/monitoring/models.py
+++ pydata_center/monitoring/models.py
@@ -8,11 +8,11 @@ class ServerStatus(models.Model):
         
     hostname = models.CharField(max_length=100)
     ip = models.GenericIPAddressField()
-    uptime = models.CharField(max_length=100)
+    uptime = models.FloatField()
     timestamp = models.DateTimeField()
     os = models.CharField(max_length=50)
     healthy = models.BooleanField(default=False)
-    service = models.CharField(max_length=50)
+    servername = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):

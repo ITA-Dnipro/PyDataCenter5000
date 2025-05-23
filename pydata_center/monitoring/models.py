6f8ca4d577b0c1b87fa77141084c3db825c1003a
Maria <marusjakarassmaria789@gmail.com>
2025-05-23 18:33:58 +0300

    Change servername to server_name

--- pydata_center/monitoring/models.py
+++ pydata_center/monitoring/models.py
@@ -12,7 +12,7 @@ class ServerStatus(models.Model):
     timestamp = models.DateTimeField()
     os = models.CharField(max_length=50)
     healthy = models.BooleanField(default=False)
-    servername = models.CharField(max_length=50)
+    server_name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):

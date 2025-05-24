d6ab85f1def47e71d5de5c6734c6c918b71735c5
YelyzavetaV <velizhaninae@gmail.com>
2025-05-24 21:15:26 +0200

    style: run pre-commit hooks

--- agents/smtp.py
+++ agents/smtp.py
@@ -1,25 +1,29 @@
-import subprocess
 import logging
+import subprocess
+
 from .agent import ServerAgent
+from .utils.logtools import maybe_log_error
 
 
 class SMTPAgent(ServerAgent):
-    server_name = "smtp"
+    server_name = 'smtp'
     port = 25
 
-    def __init__(self, smtp_processes=None):
-        super(SMTPAgent, self).__init__()
+    def __init__(self, netiface=None, smtp_processes=None):
+        super(SMTPAgent, self).__init__(netiface)
 
-        self.processes = smtp_processes or ["postfix", "exim", "sendmail", "master"]
+        self.processes = (
+            smtp_processes or ['postfix', 'exim', 'sendmail', 'master']
+        )
 
     def service_healthy(self):
         try:
             output = subprocess.Popen(
-                ["ps", "aux"], stdout=subprocess.PIPE
+                ['ps', 'aux'], stdout=subprocess.PIPE
             ).communicate()[0]
 
-            if hasattr(output, "decode"):
-                output = output.decode("utf-8")
+            if hasattr(output, 'decode'):
+                output = output.decode('utf-8')
             output = output.lower()
 
             return (
@@ -27,13 +31,10 @@ class SMTPAgent(ServerAgent):
                 and any(proc in output for proc in self.processes)
             )
         except OSError as e:
-            error_message = "SMTP check failed: %s" % str(e)
-
-            try:
-                self.logger.error(error_message)
-            except:
-                logging.getLogger(
-                    self.server_name + "_fallback"
-                ).error(error_message)
+            maybe_log_error(
+                'SMTP check failed: %s' % str(e),
+                self.logger,
+                logging.getLogger(self.server_name + '_fallback'),
+            )
 
             return False

6490440214ceee22192c73b02241f831eb571950
YelyzavetaV <velizhaninae@gmail.com>
2025-05-25 17:23:24 +0200

    docs: docstrings for helper functions

--- agents/utils/helpers.py
+++ agents/utils/helpers.py
@@ -8,13 +8,31 @@ from .logtools import maybe_log_message
 def get_config_option(
     config, section, option, default=None, logger=None, fallback_logger=None
 ):
+    """
+    Helper function to get a config option from a config parser falling
+    back to a default value if data not found. If NoSectionError or
+    NoOptionError arises and a logger is provided, logging is attempted.
+
+    Parameters:
+        config (ConfigParser.ConfigParser): Config parser.
+        section (str): Section name.
+        option (str): Option name.
+        default (Any): Fallback value. Default is None.
+        logger (logging.Logger): Primary logger. Default is None (no logging).
+        fallback_logger (logging.Logger): Fallback logger. Default is
+            None.
+
+    Returns:
+        Any: Option value.
+    """
     try:
         return config.get(section, option)
     except (ConfigParser.NoSectionError, ConfigParser.NoOptionError) as e:
-        maybe_log_message(
-            'Config not found [%s] %s: %s' % (section, option, str(e)),
-            logger,
-            fallback_logger=fallback_logger,
-            level=logging.WARNING,
-        )
+        if logger:
+            maybe_log_message(
+                'Config not found [%s] %s: %s' % (section, option, str(e)),
+                logger,
+                fallback_logger=fallback_logger,
+                level=logging.WARNING,
+            )
     return default
--- agents/utils/logtools.py
+++ agents/utils/logtools.py
@@ -4,6 +4,18 @@ import logging
 def maybe_log_message(
     message, logger, fallback_logger=None, level=logging.ERROR,
 ):
+    """
+    Helper function allowing to log a message with fallback behaviour.
+
+    Parameters:
+        message (str): Log message.
+        logger (logging.Logger): Primary logger.
+        fallback_logger (logging.Logger): Fallback logger that will try
+            logging the message if the primary logger fails due to exception.
+            Default is None. If not set, fallback behaviour will not be
+            triggered.
+        level (int): Log level. Default is logging.ERROR.
+    """
     try:
         logger.log(level, message)
     except Exception:

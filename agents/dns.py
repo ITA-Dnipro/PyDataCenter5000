c6754ffd0de5045c4684c013a3bdf4045c73620f
Svitlana Tsytsyk <TSYTSYK.svetlana@gmail.com>
2025-05-26 17:45:38 +0300

    setting port in init

--- agents/dns.py
+++ agents/dns.py
@@ -5,9 +5,9 @@ from .agent import ServerAgent
 
 class DNSAgent(ServerAgent):
     server_name = 'dns'
-    port = 53
 
     def __init__(self, dns_processes=None):
         super(DNSAgent, self).__init__()
 
+        self.port = 53
         self.processes = dns_processes or ['named', 'bind9']
--- agents/smtp.py
+++ agents/smtp.py
@@ -5,11 +5,11 @@ from .agent import ServerAgent
 
 class SMTPAgent(ServerAgent):
     server_name = 'smtp'
-    port = 25
 
     def __init__(self, smtp_processes=None):
         super(SMTPAgent, self).__init__()
 
+        self.port = 25
         self.processes = smtp_processes or [
             'postfix', 'exim', 'sendmail', 'master'
         ]

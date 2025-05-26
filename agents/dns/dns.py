bf9e88d346403fecc2540c425b619c4250bbd0cd
YelyzavetaV <velizhaninae@gmail.com>
2025-05-26 19:54:36 +0200

    refactor: move DNSAgent to its own subpackage

--- agents/dns/dns.py
+++ agents/dns/dns.py
@@ -1,9 +1,11 @@
-import logging
+import pkg_resources
 
-from .agent import ServerAgent
+from ..agent import ServerAgent
 
 
 class DNSAgent(ServerAgent):
+    config_file = pkg_resources.resource_filename(__name__, 'config.ini')
+    log_dir = pkg_resources.resource_filename(__name__, 'logs')
     server_name = 'dns'
 
     def __init__(self, dns_processes=None):

1f0ff554c8310fcb8e228d2d70ea3fbe758e7381
devdsys <dimakyrytschuk@gmail.com>
2025-05-28 18:28:20 +0300

    Add web agent

---
+++ agents/web/web.py
@@ -0,0 +1,20 @@
+import pkg_resources
+import os
+from ..agent import ServerAgent
+
+
+class WebAgent(ServerAgent):
+    config_file = pkg_resources.resource_filename(__name__, 'config.ini')
+    log_dir = pkg_resources.resource_filename(__name__, 'logs')
+    server_name = 'web'
+
+    def __init__(self, web_processes=None):
+        super(WebAgent, self).__init__()
+
+        if 'PORT' not in os.environ:
+            raise ValueError('WEB port environment variable is not set.')
+        
+        self.port = int(os.environ['PORT'])
+        self.processes = web_processes or [
+            'uvicorn',
+        ]

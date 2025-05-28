b5759be153ceca4cd05f8ca064af914f6d9b96b7
Maria <marusjakarassmaria789@gmail.com>
2025-05-28 13:28:23 +0300

    customize ServerStatusAdmin

--- pydata_center/monitoring/admin.py
+++ pydata_center/monitoring/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from .models import ServerStatus, CommandHistory
 
-admin.site.register(ServerStatus)
-
 @admin.register(CommandHistory)
 class CommandHistoryAdmin(admin.ModelAdmin):
     list_display = ('hostname', 'status', 'timestamp')
     list_filter = ('status', 'hostname')
     search_fields = ('hostname', 'command')
     readonly_fields = ('hostname', 'command', 'result', 'status', 'timestamp')
+
+
+@admin.register(ServerStatus)
+class ServerStatusAdmin(admin.ModelAdmin):
+    list_display = ('hostname', 'server_name', 'ip', 'uptime', 'timestamp')
+    search_fields = ('hostname', 'server_name')
+    list_filter = ('server_name',)
+    readonly_fields = ('hostname', 'ip', 'uptime', 'server_name', 'timestamp')
+    ordering = ('-timestamp',)

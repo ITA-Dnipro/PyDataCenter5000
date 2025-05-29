35b9e457f2a3841692032a8e6be854d6035c1707
YelyzavetaV <velizhaninae@gmail.com>
2025-05-29 12:32:26 +0200

    tests: add a test covering HTTP failure on sending status to controller

--- agents/tests/test_agent.py
+++ agents/tests/test_agent.py
@@ -2,6 +2,7 @@ import tempfile
 
 import mock
 import pytest
+import urllib2
 
 from agents.agent import ServerAgent
 
@@ -22,6 +23,7 @@ class MockAgent(ServerAgent):
 
 
 def test_type_checks_on_init():
+    """Test that type checks fail initialization with bad parameters."""
     with pytest.raises(TypeError):
         MockAgent(port='invalid')
 
@@ -30,6 +32,10 @@ def test_type_checks_on_init():
 
 
 def test_type_checks_on_config_parse():
+    """
+    Test that type checks fail initialization with bad config file
+    parameters.
+    """
     with tempfile.NamedTemporaryFile() as tmp:
         tmp.write('[server]\nname=mock\nport=invalid\nprocesses=proc1')
         tmp.flush()
@@ -39,6 +45,10 @@ def test_type_checks_on_config_parse():
 
 
 def test_status_to_controller_success():
+    """
+    Test that successful POST request to controller is properly handled
+    and logged.
+    """
     def mock_urlopen(request, timeout=5):
         class MockResponse(object):
             def getcode(self):
@@ -60,4 +70,41 @@ def test_status_to_controller_success():
 
         assert 'POST request status: 201' in contents, (
             'Expected "POST request status: 201" in logs, got:\n%s' % contents
+        )
+
+
+def test_status_to_controller_http_error():
+    """
+    Test that the HTTP failure of POST request to controller is properly
+    handled and logged.
+    """
+    def mock_urlopen(request, timeout=5):
+        raise urllib2.HTTPError(
+            url='http://mock/api/status/',
+            code=500,
+            msg='Internal Server Error',
+            hdrs=None,
+            fp=None,
+        )
+
+    with tempfile.NamedTemporaryFile() as tmp:
+        agent = MockAgent(port=12345)
+        agent.setup_logging(tmp.name)
+        agent.collect_server_metadata()
+
+        agent.controller_url = 'http://mock/api/status/'
+
+        with mock.patch('urllib2.urlopen', mock_urlopen):
+            agent.status_to_controller()
+
+        tmp.seek(0)
+        contents = tmp.read()
+
+        msg = (
+            'POST request to controller failed due to error: '
+            'HTTP Error 500: Internal Server Error'
+        )
+
+        assert msg in contents, (
+            'Expected %s in logs, got:\n%s' % (msg, contents)
         )

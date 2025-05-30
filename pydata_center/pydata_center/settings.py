9b6bd67b7282e7af473167c24e69549a03faad4b
Volodymyr Povroznyk <vpovroznyk1998@gmail.com>
2025-05-30 15:08:24 +0300

    Add templates and static directories for monitoring app; update settings.py accordingly

--- pydata_center/pydata_center/settings.py
+++ pydata_center/pydata_center/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'pydata_center.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'monitoring' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -149,3 +149,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'monitoring' / 'static',
+]

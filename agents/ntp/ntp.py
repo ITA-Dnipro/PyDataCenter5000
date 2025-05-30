52d92106ebe639dce372d58fe8c9b88f53719071
Bohdan Khudiakov <b.khudiakov@gmail.com>
2025-05-30 20:46:56 +0300

    Modified agents/ntp/config.ini and agents/ntp/ntp.py

--- agents/ntp/ntp.py
+++ agents/ntp/ntp.py
@@ -1,22 +1,24 @@
-import socket
-
-import pkg_resources
-
 from ..agent import ServerAgent
 
 
 class NTPAgent(ServerAgent):
-    config_file = pkg_resources.resource_filename(__name__, 'config.ini')
-    log_dir = pkg_resources.resource_filename(__name__, 'logs')
-    server_name = 'ntp'
 
     protocol = 'udp'
     udp_probe_payload = b'\x1b' + 47 * b'\0'
     udp_probe_response_len = 48
 
-    def __init__(self, ntp_processes=None):
-        super(NTPAgent, self).__init__()
-        self.port = 123
-        self.processes = ntp_processes or [
-            'ntpd', 'chronyd', 'systemd-timesyncd'
-        ]
+    def __init__(
+        self,
+        server_name='ntp',
+        port=123,
+        processes=None,
+        interface=None,
+        controller_url=None,
+    ):
+        super(NTPAgent, self).__init__(
+            server_name=server_name,
+            port=port,
+            processes=processes or ['ntpd', 'chronyd', 'systemd-timesyncd'],
+            interface=interface,
+            controller_url=controller_url,
+        )

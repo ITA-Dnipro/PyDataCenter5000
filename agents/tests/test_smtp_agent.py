b56282b9759f26f7db09330d1ee9c31df7d8cfcf
Maria <marusjakarassmaria789@gmail.com>
2025-06-03 14:48:38 +0300

    update SMTPAgent test to use agent.ip and setup_logging

--- agents/tests/test_smtp_agent.py
+++ agents/tests/test_smtp_agent.py
@@ -5,7 +5,9 @@ from agents.smtp.smtp import SMTPAgent
 
 def test_banner_check(host='127.0.0.1'):
     agent = SMTPAgent()
-    result = agent.check_banner(host=host)
+    agent.ip = host  # send ip via attribute
+    agent.setup_logging()  # to avoid logger mistake
+    result = agent.check_banner()
 
     assert 'status' in result
     assert result['status'] in ('ok', 'no banner', 'error')

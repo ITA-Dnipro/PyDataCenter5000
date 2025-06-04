9af923ec448e05f7fb236aa9dd9e625969eb8c4e
YelyzavetaV <velizhaninae@gmail.com>
2025-06-04 13:39:55 +0200

    feat: add admin interface for AlertRule

--- pydata_center/monitoring/admin.py
+++ pydata_center/monitoring/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import CommandHistory, ServerStatus
+from .models import AlertRule, CommandHistory, ServerStatus
 
 
 @admin.register(CommandHistory)
@@ -14,7 +14,22 @@ class CommandHistoryAdmin(admin.ModelAdmin):
 @admin.register(ServerStatus)
 class ServerStatusAdmin(admin.ModelAdmin):
     list_display = ('hostname', 'server_name', 'ip', 'uptime', 'timestamp')
-    search_fields = ('hostname', 'server_name')
     list_filter = ('server_name', )
+    search_fields = ('hostname', 'server_name')
     readonly_fields = ('hostname', 'ip', 'uptime', 'server_name', 'timestamp')
     ordering = ('-timestamp', )
+
+
+@admin.register(AlertRule)
+class AlertRuleAdmin(admin.ModelAdmin):
+    list_display = (
+        'metric',
+        'operator',
+        'threshold',
+        'time_window_minutes',
+        'frequency',
+        'is_active',
+    )
+    list_filter = ('metric', 'is_active')
+    search_fields = ('metric', )
+    readonly_fields = ('created_at', )

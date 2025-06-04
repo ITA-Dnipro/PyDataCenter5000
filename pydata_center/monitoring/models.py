f4d6b306a7c1fef1e77989ef46caf92d23cd3406
YelyzavetaV <velizhaninae@gmail.com>
2025-06-04 13:13:37 +0200

    feat: added AlertRule model and created the corresponding migration file

--- pydata_center/monitoring/models.py
+++ pydata_center/monitoring/models.py
@@ -5,7 +5,7 @@ class ServerStatus(models.Model):
 
     class Meta:
         indexes = [
-            models.Index(fields=['hostname', 'timestamp']),
+            models.Index(fields=['hostname', 'timestamp'])
         ]
 
     hostname = models.CharField(max_length=100)
@@ -41,3 +41,43 @@ class CommandHistory(models.Model):
 
     def __str__(self):
         return f'{self.hostname} - {self.status} - {self.timestamp}'
+
+
+class AlertRule(models.Model):
+
+    class Meta:
+        indexes = [models.Index(fields=['metric', 'is_active'])]
+
+    METRIC_CHOICES = [
+        ('cpu', 'CPU Usage'),
+        ('ram', 'RAM Usage'),
+        ('disk', 'Disk Usage'),
+        ('nginx_down_count', 'Nginx Failure Count'),
+        ('uptime', 'Uptime'),
+    ]
+    OPERATOR_CHOICES = [
+        ('>', 'Greater than'),
+        ('<', 'Less than'),
+        ('==', 'Equal to'),
+    ]
+
+    metric = models.CharField(max_length=32, choices=METRIC_CHOICES)
+    operator = models.CharField(max_length=2, choices=OPERATOR_CHOICES)
+    threshold = models.FloatField()
+    time_window_minutes = models.IntegerField(default=5)
+    frequency = models.IntegerField(
+        help_text='Check system every N minutes', default=5
+    )
+    is_active = models.BooleanField(default=True)
+
+    notify_message = models.CharField(
+        max_length=255, default='Alert triggered!'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f'{self.metric} {self.operator} {self.threshold} '
+            f'over {self.time_window_minutes}m'
+        )

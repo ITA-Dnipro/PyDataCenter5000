00ec1b7c6c8c95042d478f45b89a68d19036383a
Svitlana Tsytsyk <TSYTSYK.svetlana@gmail.com>
2025-06-04 20:47:44 +0300

    added AgentMetric model & migration

--- pydata_center/monitoring/models.py
+++ pydata_center/monitoring/models.py
@@ -11,7 +11,7 @@ class ServerStatus(models.Model):
     hostname = models.CharField(max_length=100)
     ip = models.GenericIPAddressField()
     uptime = models.FloatField()
-    timestamp = models.DateTimeField()
+    timestamp = models.DateTimeField(auto_now_add=True)
     os = models.CharField(max_length=50)
     healthy = models.BooleanField(default=False)
     server_name = models.CharField(max_length=50)
@@ -19,6 +19,26 @@ class ServerStatus(models.Model):
 
     def __str__(self):
         return f'{self.hostname} - {self.timestamp}'
+
+
+class AgentMetric(models.Model):
+    cpu = models.FloatField(null=True, blank=True)
+    ram = models.FloatField(null=True, blank=True)
+    disk = models.FloatField(null=True, blank=True)
+    load_avg = models.FloatField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    server_status = models.ForeignKey(
+        ServerStatus, on_delete=models.CASCADE, related_name='server_status'
+    )
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['server_status', 'timestamp']),
+        ]
+
+    def __str__(self):
+        return f'Metrics for {self.server_status.hostname} at {self.timestamp}'
 
 
 class CommandHistory(models.Model):

6fb44181136868c46e5e6d9bf6d9447c49973f37
Maria <marusjakarassmaria789@gmail.com>
2025-06-05 14:21:04 +0300

    assign smtp_agent logger and fallback_logger to agent instance in text fixture

--- agents/tests/test_smtp_agent.py
+++ agents/tests/test_smtp_agent.py
@@ -33,6 +33,9 @@ def smtp_agent():
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
+
+    agent.logger = logger
+    agent.fallback_logger = logger
     
     yield agent, logfile.name
 

7f3e34a490b472e886ad017d5bb1d3cb4267fd6c
Volodymyr Povroznyk <vpovroznyk1998@gmail.com>
2025-06-09 19:34:10 +0300

    Integrate Discord bot into Django via AppConfig.ready()

--- pydata_center/monitoring/apps.py
+++ pydata_center/monitoring/apps.py
@@ -1,6 +1,12 @@
+import threading
+
 from django.apps import AppConfig
 
 
 class MonitoringConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'monitoring'
+
+    def ready(self):
+        from .discord_bot import start_discord_bot
+        threading.Thread(target=start_discord_bot, daemon=True).start()

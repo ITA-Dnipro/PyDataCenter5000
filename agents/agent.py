b7749db4d38140a91889f0ed104844ab7f481f64
YelyzavetaV <velizhaninae@gmail.com>
2025-06-12 17:32:36 +0300

    feat: use timestamps in event queue for ordering

--- agents/agent.py
+++ agents/agent.py
@@ -7,7 +7,6 @@ import platform
 import socket
 import subprocess
 import time
-import uuid
 from collections import Sequence
 
 import attr
@@ -732,4 +731,4 @@ class ServerAgent(object):
             return
 
         if command_history.command in self.whitelist_commands:
-            self.queue.insert(uuid.uuid4().int, command_history)
+            self.queue.insert(time.time() * 1000, command_history)

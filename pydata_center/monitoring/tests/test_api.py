dfc79161dc0abb6e3b3ee202e6305c97dc7bc396
Maria <marusjakarassmaria789@gmail.com>
2025-06-17 13:27:00 +0300

    fix

--- pydata_center/monitoring/tests/test_api.py
+++ pydata_center/monitoring/tests/test_api.py
@@ -534,19 +534,19 @@ class TestEvaluateAgentAlerts:
     @pytest.mark.parametrize(
         ['destination', 'mocked'],
         [
-            (   
-                'email', 
-                'monitoring.email.send_async_email_message.apply_async'
+            (
+                'email',
+                'monitoring.email.send_async_email_message.apply_async',
             ),
-            (   
-                'discord', 
-                'monitoring.discord.send_async_discord_message.apply_async'
+            (
+                'discord',
+                'monitoring.discord.send_async_discord_message.apply_async',
             ),
-            (   
-                'slack', 
-                'monitoring.webhook.send_async_webhook_message.apply_async'
+            (
+                'slack',
+                'monitoring.webhook.send_async_webhook_message.apply_async',
             ),
-        ]
+        ],
     )
     def test_alert_triggered(self, destination, mocked, caplog):
         with caplog.at_level('WARNING'), patch(mocked) as mock_send_message:

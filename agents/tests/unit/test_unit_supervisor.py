221b2959ee20c1912a5e948715dd5b698213c611
YelyzavetaV <velizhaninae@gmail.com>
2025-06-19 20:47:01 +0300

    tests: add unit test for negative sleep interval

--- agents/tests/unit/test_unit_supervisor.py
+++ agents/tests/unit/test_unit_supervisor.py
@@ -73,6 +73,23 @@ def test_unit_start_event_loop_with_no_tasks_logged(
 
 
 @pytest.mark.coro
+def test_sleep_negative_interval_logged(mock_supervisor):
+    mock_supervisor.sleep(-1)
+
+    with open(mock_supervisor.logfile.name, 'r') as f:
+        f.seek(0)
+        contents = f.read()
+
+    msg = 'Negative sleep interval not allowed'
+
+    assert msg in contents, (
+        'Expected log message %s not found. Log contents:\n %s' % (
+            msg, contents
+        )
+    )
+
+
+@pytest.mark.coro
 def test_schedule_unschedule_coro_logged(mock_supervisor):
     """Test task scheduling and unscheduling with supervisor."""
     def mock_schedule_task(*args, **kwargs):

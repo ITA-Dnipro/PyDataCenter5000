c6dda72c2c94026e90f74e8ed8b02446c334c896
Maria <marusjakarassmaria789@gmail.com>
2025-06-20 16:16:20 +0300

    fix Slack webhook tests with missing ALERT_SLACK_WEBHOOK

---
+++ pydata_center/monitoring/tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from django.test.utils import override_settings
+
+@pytest.fixture(autouse=True)
+def slack_webhook_setting():
+    with override_settings(
+        ALERT_SLACK_WEBHOOK='https://hooks.slack.com/services/test/test/test'
+    ):
+        yield

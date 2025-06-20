c5f2c1c9198d7191765c46a8cbc3faa33c3e20ab
Mariia Novytska <mnovickaa@gmail.com>
2025-06-20 14:55:21 +0300

    Added maybe_restart_service() to all agents

--- agents/ntp/ntp.py
+++ agents/ntp/ntp.py
@@ -1,4 +1,8 @@
+import logging
+
 from ..agent import ServerAgent
+from ..utils.helpers import restart_service
+from ..utils.logtools import maybe_log_message
 
 
 class NTPAgent(ServerAgent):
@@ -32,3 +36,29 @@ class NTPAgent(ServerAgent):
         return status and self.is_port_open(
                 timeout=timeout, payload=payload, packet_size=packet_size
             )
+
+    def maybe_restart_service(self):
+        inactive_services = []
+
+        if not self.is_ssh_service_active():
+            inactive_services.append('ssh')
+
+        if inactive_services:
+            for service in inactive_services:
+                restart_service(self.logger, self.fallback_logger, service)
+
+            maybe_log_message(
+                'Finished attempts to restart services',
+                self.logger,
+                fallback_logger=self.fallback_logger,
+                level=logging.INFO
+                )
+            return False
+
+        maybe_log_message(
+            'All services are heathy and running',
+            self.logger,
+            fallback_logger=self.fallback_logger,
+            level=logging.INFO
+            )
+        return True

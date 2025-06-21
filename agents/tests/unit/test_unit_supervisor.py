14bb13d1b1f85037bc77d14fdba7fc5ddacf61fd
YelyzavetaV <velizhaninae@gmail.com>
2025-06-21 20:58:00 +0300

    chore: update unit supervisor tests to use fixture for checking logs

--- agents/tests/unit/test_unit_supervisor.py
+++ agents/tests/unit/test_unit_supervisor.py
@@ -3,44 +3,34 @@ import pytest
 
 @pytest.mark.coro
 def test_unit_start_event_loop_with_no_tasks_logged(
-    dummy_supervisor, monkeypatch
+    monkeypatch,
+    dummy_supervisor,
+    setup_temp_file_logging_with_fallback,
+    assert_msg_in_logfile
 ):
     monkeypatch.setattr('coro.event_loop', lambda timeout: None)
 
     dummy_supervisor.start()
 
-    with open(dummy_supervisor.logger.handlers[0].baseFilename, 'r') as f:
-        f.seek(0)
-        contents = f.read()
-
-    msg = 'Coroutine queue is empty'
-
-    assert msg in contents, (
-        'Expected log message %s not found. Log contents:\n %s' % (
-            msg, contents
-        )
-    )
+    assert_msg_in_logfile('Coroutine queue is empty')
 
 
 @pytest.mark.coro
-def test_sleep_negative_interval_logged(dummy_supervisor):
+def test_sleep_negative_interval_logged(
+    dummy_supervisor,
+    setup_temp_file_logging_with_fallback,
+    assert_msg_in_logfile
+):
     dummy_supervisor.sleep(-1)
-
-    with open(dummy_supervisor.logger.handlers[0].baseFilename, 'r') as f:
-        f.seek(0)
-        contents = f.read()
-
-    msg = 'Negative sleep interval not allowed'
-
-    assert msg in contents, (
-        'Expected log message %s not found. Log contents:\n %s' % (
-            msg, contents
-        )
-    )
+    assert_msg_in_logfile('Negative sleep interval not allowed')
 
 
 @pytest.mark.coro
-def test_schedule_unschedule_coro_logged(dummy_supervisor):
+def test_schedule_unschedule_coro_logged(
+    dummy_supervisor,
+    setup_temp_file_logging_with_fallback,
+    assert_msg_in_logfile
+):
     """Test task scheduling and unscheduling with supervisor."""
     def mock_schedule_task(*args, **kwargs):
         pass
@@ -51,21 +41,15 @@ def test_schedule_unschedule_coro_logged(dummy_supervisor):
     dummy_supervisor.unschedule(idx)
     assert dummy_supervisor.get_coro(idx, log=True) is None
 
-    with open(dummy_supervisor.logger.handlers[0].baseFilename, 'r') as f:
-        f.seek(0)
-        contents = f.read()
-
-    msg = 'Coroutine %d not in tasks' % idx
-
-    assert msg in contents, (
-        'Expected log message %s not found. Log contents:\n %s' % (
-            msg, contents
-        )
-    )
+    assert_msg_in_logfile('Coroutine %d not in tasks' % idx)
 
 
 @pytest.mark.coro
-def test_schedule_weak_coro_logged(dummy_supervisor):
+def test_schedule_weak_coro_logged(
+    dummy_supervisor,
+    setup_temp_file_logging_with_fallback,
+    assert_msg_in_logfile
+):
     """Test proper scheduling of a 'weak' coro."""
     def mock_weak_task(*args, **kwargs):
         pass
@@ -73,21 +57,15 @@ def test_schedule_weak_coro_logged(dummy_supervisor):
     idx = dummy_supervisor.schedule(mock_weak_task, weak=True)
     assert dummy_supervisor.get_coro(idx, log=True) is None
 
-    with open(dummy_supervisor.logger.handlers[0].baseFilename, 'r') as f:
-        f.seek(0)
-        contents = f.read()
-
-    msg = 'Coroutine %d not in tasks' % idx
-
-    assert msg in contents, (
-        'Expected log message %s not found. Log contents:\n %s' % (
-            msg, contents
-        )
-    )
+    assert_msg_in_logfile('Coroutine %d not in tasks' % idx)
 
 
 @pytest.mark.coro
-def test_unschedule_nonexistent_coro_logged(dummy_supervisor):
+def test_unschedule_nonexistent_coro_logged(
+    dummy_supervisor,
+    setup_temp_file_logging_with_fallback,
+    assert_msg_in_logfile
+):
     """
     Test proper handling and logging of trying to unschedule nonexistent
     coro.
@@ -98,21 +76,15 @@ def test_unschedule_nonexistent_coro_logged(dummy_supervisor):
 
     dummy_supervisor.unschedule(-1)
 
-    with open(dummy_supervisor.logger.handlers[0].baseFilename, 'r') as f:
-        f.seek(0)
-        contents = f.read()
-
-    msg = 'Coroutine -1 not in tasks'
-
-    assert msg in contents, (
-        'Expected log message %s not found. Log contents:\n %s' % (
-            msg, contents
-        )
-    )
+    assert_msg_in_logfile('Coroutine -1 not in tasks')
 
 
 @pytest.mark.coro
-def test_double_unschedule_coro_logged(dummy_supervisor):
+def test_double_unschedule_coro_logged(
+    dummy_supervisor,
+    setup_temp_file_logging_with_fallback,
+    assert_msg_in_logfile
+):
     """
     Test proper handling and logging of trying to unschedule the same
     coro twice.
@@ -131,17 +103,7 @@ def test_double_unschedule_coro_logged(dummy_supervisor):
         'Unexpected coroutine found in the scheduler'
     )
 
-    with open(dummy_supervisor.logger.handlers[0].baseFilename, 'r') as f:
-        f.seek(0)
-        contents = f.read()
-
-    msg = 'Coroutine %d not in tasks' % idx
-
-    assert msg in contents, (
-        'Expected log message %s not found. Log contents:\n %s' % (
-            msg, contents
-        )
-    )
+    assert_msg_in_logfile('Coroutine %d not in tasks' % idx)
 
 
 @pytest.mark.coro
@@ -154,7 +116,11 @@ def test_exit_task_scheduled(dummy_supervisor):
 
 
 @pytest.mark.coro
-def test_exit_task_scheduled_twice_logged(dummy_supervisor):
+def test_exit_task_scheduled_twice_logged(
+    dummy_supervisor,
+    setup_temp_file_logging_with_fallback,
+    assert_msg_in_logfile
+):
     dummy_supervisor.schedule_exit()
     dummy_supervisor.schedule_exit()
 
@@ -162,16 +128,6 @@ def test_exit_task_scheduled_twice_logged(dummy_supervisor):
         'Failed to schedule exit task'
     )
 
-    with open(dummy_supervisor.logger.handlers[0].baseFilename, 'r') as f:
-        f.seek(0)
-        contents = f.read()
-
-    msg = (
+    assert_msg_in_logfile(
         'Exit coroutine is already in task - only one at a time is permitted'
-    )
-
-    assert msg in contents, (
-        'Expected log message %s not found. Log contents:\n %s' % (
-            msg, contents
-        )
     )

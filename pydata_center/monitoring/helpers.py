c3624da7f0fc13209cdb55a93dc70629c46465a3
Volodymyr Povroznyk <vpovroznyk1998@gmail.com>
2025-06-25 18:14:26 +0300

    Implement filtering by tags functionality for the agent dashboard based on URL query parameters

--- pydata_center/monitoring/helpers.py
+++ pydata_center/monitoring/helpers.py
@@ -6,15 +6,23 @@ from django.utils.timezone import now
 from .models import ServerStatus
 
 
-def get_latest_agents(cutoff_seconds=60):
+def get_latest_agents(query_params=None, cutoff_seconds=60):
     cutoff_time = now() - timedelta(seconds=cutoff_seconds)
+
+    tag_filters = {}
+    if query_params:
+        for key, value in query_params.items():
+            if key.startswith('tag_') and value:
+                filter_key = f'tags__{key[4:]}__icontains'
+                tag_filters[filter_key] = value
 
     latest_subquery = ServerStatus.objects.filter(
         hostname=OuterRef('hostname')
     ).order_by('-timestamp').values('pk')[:1]
 
     latest_statuses = ServerStatus.objects.filter(
-        pk=Subquery(latest_subquery)
+        pk=Subquery(latest_subquery),
+        **tag_filters
     ).order_by('hostname')
 
     return [

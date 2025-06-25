cf12cb0ecb5a0c96c4c982b53e09b19ed2de713e
Maria <marusjakarassmaria789@gmail.com>
2025-06-25 16:03:38 +0300

    update naming

--- pydata_center/monitoring/tests/conftest.py
+++ pydata_center/monitoring/tests/conftest.py
@@ -3,7 +3,7 @@ from django.test.utils import override_settings
 
 
 @pytest.fixture(autouse=True)
-def slack_webhook_setting():
+def mock_webhook_settings():
     with override_settings(
         ALERT_SLACK_WEBHOOK='https://hooks.slack.com/services/test/test/test'
     ):

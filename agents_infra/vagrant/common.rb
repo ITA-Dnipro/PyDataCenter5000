1d8b00da999ac94b4d142f9eedc96fdb7a3f554e
devdsys <dimakyrytschuk@gmail.com>
2025-06-26 16:23:48 +0300

    chore: move /scripts/ to the root folder

--- agents_infra/vagrant/common.rb
+++ agents_infra/vagrant/common.rb
@@ -3,6 +3,7 @@ module VagrantCommon
     config.vm.box = env["OS"]
     config.vm.hostname = env["HOSTNAME"]
     config.vm.synced_folder "agents_infra/agents_infra", "/home/vagrant/agents_infra"
+    config.vm.synced_folder "agents_infra/scripts", "/home/vagrant/scripts"
     if env["VM_ARCH"]
       config.vm.box_architecture = env["VM_ARCH"]
     end
@@ -15,7 +16,7 @@ module VagrantCommon
 
   def self.base_provision
     <<-SHELL
-      cd /home/vagrant/agents_infra/scripts
+      cd /home/vagrant/scripts
       chmod +x setup-server.sh install-dependencies.sh
       bash setup-server.sh && bash install-dependencies.sh
       cp /home/vagrant/agents_infra/.coveragerc /home/vagrant/.coveragerc

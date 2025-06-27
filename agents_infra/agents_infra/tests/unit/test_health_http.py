2bfce9062da188d0d0ab103156878c8f31a5b144
Svitlana Tsytsyk <TSYTSYK.svetlana@gmail.com>
2025-06-27 23:12:51 +0300

    fixed imports in test folders

--- agents_infra/agents_infra/tests/unit/test_health_http.py
+++ agents_infra/agents_infra/tests/unit/test_health_http.py
@@ -5,8 +5,8 @@ import unittest
 import mock
 import StringIO
 
-from ..managers.health_server_manager import HealthServerManager
-from ..utils.health_http import HealthHandler
+from ...managers.health_server_manager import HealthServerManager
+from ...utils.health_http import HealthHandler
 
 
 class DummyServer(object):

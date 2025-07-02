8fae528790196ead67609446e50cf30dd7b53a83
Svitlana Tsytsyk <TSYTSYK.svetlana@gmail.com>
2025-07-02 11:17:35 +0300

    fixed import

--- agents_infra/agents_infra/utils/__init__.py
+++ agents_infra/agents_infra/utils/__init__.py
@@ -1,3 +1,2 @@
-from .logtools import (LOG_CONFIG_PATH, ControllerLogHandler,
-                       get_fallback_logger, maybe_log_message)
+from .logtools import LOG_CONFIG_PATH, get_fallback_logger, maybe_log_message
 from .retry import jitter, make_callback

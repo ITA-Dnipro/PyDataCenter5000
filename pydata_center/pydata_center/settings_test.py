a72eb782d696515cba3fdee9d51fb928802d5eab
Bohdan Khudiakov <b.khudiakov@gmail.com>
2025-07-04 15:02:16 +0300

    Remove 'DEFAULT_AUTHENTICATION_CLASSES' and 'DEFAULT_PERMISSION_CLASSES' from 'settings_test.py'

--- pydata_center/pydata_center/settings_test.py
+++ pydata_center/pydata_center/settings_test.py
@@ -2,13 +2,8 @@ from datetime import timedelta
 
 from .settings import *  # noqa: F403
 
-REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = []  # noqa: F405
-REST_FRAMEWORK['DEFAULT_PERMISSION_CLASSES'] = [  # noqa: F405
-    'rest_framework.permissions.AllowAny',
-]
 REST_FRAMEWORK['DEFAULT_THROTTLE_CLASSES'] = []  # noqa: F405
 REST_FRAMEWORK['DEFAULT_THROTTLE_RATES'] = {}  # noqa: F405
-
 
 SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'] = timedelta(days=7)  # noqa: F405
 SIMPLE_JWT['REFRESH_TOKEN_LIFETIME'] = timedelta(days=30)  # noqa: F405

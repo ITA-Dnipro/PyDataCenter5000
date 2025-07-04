9615a143d707e2355440a9e4b4763022ade04b41
Volodymyr Povroznyk <vpovroznyk1998@gmail.com>
2025-07-04 15:20:58 +0300

    Register URL route for set_agent_tags endpoint

--- pydata_center/monitoring/urls.py
+++ pydata_center/monitoring/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import (CommandHistoryViewSet, TriggeredAlertViewSet,
                     create_agent_metric, dashboard_view, fetch_pending_command,
                     metrics_graphing_page, metrics_history_view,
-                    receive_status, submit_command_result)
+                    receive_status, set_agent_tags, submit_command_result)
 
 app_name = 'monitoring'
 
@@ -37,4 +37,9 @@ urlpatterns = [
     ),
     path('metrics/history/', metrics_history_view, name='metrics_history'),
     path('metrics/graphic/', metrics_graphing_page, name='metrics_graphic'),
+    path(
+        'agents/<str:hostname>/set-tags/',
+        set_agent_tags,
+        name='set_agent_tags'
+    ),
 ]

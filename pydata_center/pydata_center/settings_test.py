31cd680e25d275d6935416778abc7ca0384e2770
Bohdan Khudiakov <b.khudiakov@gmail.com>
2025-07-07 22:23:05 +0300

    config: set ALLOWED_HOSTS to '*' to enable external access through Toxiproxy

--- pydata_center/pydata_center/settings_test.py
+++ pydata_center/pydata_center/settings_test.py
@@ -8,4 +8,4 @@ REST_FRAMEWORK['DEFAULT_THROTTLE_RATES'] = {}  # noqa: F405
 SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'] = timedelta(days=7)  # noqa: F405
 SIMPLE_JWT['REFRESH_TOKEN_LIFETIME'] = timedelta(days=30)  # noqa: F405
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['*']

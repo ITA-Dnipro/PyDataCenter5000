1e6984c6fbf02e9b7b5cf4d8b83efdef1cafad2f
Mariia Novytska <mnovickaa@gmail.com>
2025-07-08 15:27:54 +0300

    commit before merge develop

--- agents_infra/agents_infra/dev/start_dns.py
+++ agents_infra/agents_infra/dev/start_dns.py
@@ -3,11 +3,22 @@ from agents_infra.agents.dns.dns import DNSAgentNamed
 
 config_path = '/vagrant/agents_infra/agents_infra/agents/dns/config.ini'
 
+# config_dict = {
+#     'name': 'dns_named',
+#     'api_prefix': 'api/v1/',
+#     'url': 'http://localhost:8000/',
+#     'critical_processes': ['named'],
+#     'whitelist_commands': ['dig', 'whoami', 'ls'],
+#     'port': 53,
+#     'auth_token_type': None,
+#     'interface': 'enp0s3',
+# }
 # Creating DNS agent
 agent = DNSAgentNamed.from_config_file(filename=config_path)
 
 # Collect metadata
 agent.collect_server_metadata()
+agent._are_all_critical_processes_active(restart=True)
 
 # Prints as a dict
 status = agent.status_to_dict()

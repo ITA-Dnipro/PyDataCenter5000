d0e1a126e744b4a69cfc32381f2dcdae42b8c6c9
YelyzavetaV <velizhaninae@gmail.com>
2025-07-08 14:46:45 +0300

    chore: simulate application

--- agents_infra/main.py
+++ agents_infra/main.py
@@ -1,8 +1,19 @@
-from agents_infra import NTPAgent
+import logging
+import time
+
+logger = logging.getLogger(__name__)
 
 
 def main():
-    NTPAgent.from_config_file()
+    logging.info('agents-infra started. Press Ctrl+C to exit.')
+
+    try:
+        while True:
+            time.sleep(5)
+    except KeyboardInterrupt:
+        logging.info('Interrupting...')
+    finally:
+        logging.info('agents-infra exiting...')
 
 
 if __name__ == '__main__':

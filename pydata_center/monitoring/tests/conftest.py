7a4bd63c2489b5140c7288fd4f624359da21a5fb
Maria <marusjakarassmaria789@gmail.com>
2025-07-14 17:29:10 +0300

    fix conftest

--- pydata_center/monitoring/tests/conftest.py
+++ pydata_center/monitoring/tests/conftest.py
@@ -15,14 +15,24 @@ def celery_includes():
     return ['monitoring.tasks']
 
 
-@pytest.fixture(scope='session')
-def celery_worker_parameters():
-    return {'use_local_worker': True}
-
-
 @pytest.fixture(autouse=True)
 def mock_webhook_settings():
     with override_settings(
         ALERT_SLACK_WEBHOOK='https://hooks.slack.com/services/test/test/test'
     ):
         yield
+
+
+@pytest.fixture(scope='function')
+def celery_worker(celery_app):
+    """
+    Override the default celery_worker so it does NOT build a Docker image,
+    and instead just yields celery_app in the same process.
+    """
+    yield celery_app
+
+
+@pytest.fixture(autouse=True)
+def celery_always_eager(settings):
+    settings.CELERY_TASK_ALWAYS_EAGER = True
+    settings.CELERY_TASK_EAGER_PROPAGATES = True

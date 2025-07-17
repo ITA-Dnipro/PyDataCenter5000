dca981966b96b7954930923984622dc76074f520
devdsys <dimakyrytschuk@gmail.com>
2025-07-17 15:52:27 +0300

    configure SSH to start in /app directory

--- agents_infra/scripts/provision/setup_ssh.sh
+++ agents_infra/scripts/provision/setup_ssh.sh
@@ -6,3 +6,5 @@ SSH_PASSWORD="$1"
 mkdir /var/run/sshd
 echo "root:${SSH_PASSWORD}" | chpasswd
 sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+echo "cd /app" >> /root/.bashrc

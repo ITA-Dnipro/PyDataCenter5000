7257cdfc8cc6a705dcf21cb58b4fc6fae8c12258
ne0ascorbinka <ne.ascorbinka@gmail.com>
2025-07-28 18:16:43 +0300

    feat(tasks): load task configurations from JSON file and template

--- agents_infra/agents_infra/main.py
+++ agents_infra/agents_infra/main.py
@@ -1,4 +1,4 @@
-import base64
+import json
 import logging
 import os
 import sys
@@ -8,28 +8,16 @@ from agents_infra.supervisor import AgentSupervisor
 from agents_infra.tasks import periodic_task_wrapper, task_factory
 from agents_infra.utils.logtools import maybe_log_message
 
-DEBUG = True  # Set to True to run tasks directly for testing
 
-# Hardcoded config for tasks
-TASKS_CONFIG = [
-    {
-        'name': 'collect_and_send_status',
-        'interval': 5,
-        'endpoint': 'server/status/'
-    }, {
-        'name': 'collect_and_send_metrics',
-        'interval': 5,
-        'endpoint': 'agent/metrics/'
-    }, {
-        'name': 'fetch_command',
-        'interval': 3,
-        'endpoint': None
-    }, {
-        'name': 'execute_command',
-        'interval': 2,
-        'endpoint': None
-    }
-]
+def load_tasks_config():
+    config_paths = [
+        os.path.join(os.path.dirname(__file__), 'tasks_config.json'),
+        os.path.join(os.path.dirname(__file__), 'tasks_config.json.template'),
+    ]
+    for path in config_paths:
+        if os.path.exists(path):
+            with open(path, 'r') as f:
+                return json.load(f)
 
 
 class AgentApp(object):
@@ -38,7 +26,7 @@ class AgentApp(object):
         self.agent_name = agent_name
         self.agent = agent_factory(agent_name)
         self.supervisor = AgentSupervisor(self.agent)
-        self.task_configs = task_configs or TASKS_CONFIG
+        self.task_configs = task_configs or load_tasks_config()
         self.tasks = self.create_tasks()
 
     def create_tasks(self):
@@ -66,25 +54,8 @@ def main():
         )
         sys.exit(1)
     agent_name = sys.argv[1]
-    if DEBUG:
-        agent = agent_factory(agent_name)
-        print('agent.config.url: %s' % agent.config.url)
-        print('agent.config.url[repr]: %s' % repr(agent.config.url))
-        tasks = [task_factory(cfg, agent) for cfg in TASKS_CONFIG]
-        for task in tasks:
-            try:
-                print('Running task: %s' % task.__class__.__name__)
-                result = task()
-                print(
-                    'Task %s completed successfully. Result: %s' %
-                    (task.__class__.__name__, result)
-                )
-            except Exception as e:
-                print('Task %s failed: %s' % (task.__class__.__name__, e))
-        print('Exiting...')
-    else:
-        runner = AgentApp(agent_name)
-        runner.run()
+    runner = AgentApp(agent_name, task_configs=load_tasks_config())
+    runner.run()
 
 
 if __name__ == '__main__':

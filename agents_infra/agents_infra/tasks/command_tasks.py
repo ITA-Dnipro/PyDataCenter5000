f28736eb3ba39abec35a56696cd021873b145c26
ne0ascorbinka <ne.ascorbinka@gmail.com>
2025-07-28 17:19:48 +0300

    fix(agent, tasks): adjust tasks implementation to new agent API

--- agents_infra/agents_infra/tasks/command_tasks.py
+++ agents_infra/agents_infra/tasks/command_tasks.py
@@ -1,23 +1,37 @@
+import json
 import logging
+import urllib
 
 from agents_infra.tasks.basetask import BaseTask
 from agents_infra.utils.auth import Authentication
 from agents_infra.utils.logtools import maybe_log_message
 
 
+def _build_url_with_params(base_url, params):
+    if not params:
+        return base_url
+    return base_url + '?' + urllib.urlencode(params)
+
+
 class FetchCommandTask(BaseTask):
 
     def handle(self):
-        data = self.agent.fetch_command_from_controller(
-            api_key=Authentication.get_key()
+        url = _build_url_with_params(self.endpoint,
+                                     {'hostname': self.agent.hostname})
+        data = self.agent.get_data(
+            url=url,
+            max_retries=1,
+            fail_silently=False,
+            api_key=Authentication.get_key(),
         )
         if data:
             maybe_log_message(
-                'Fetched command from controller: %s' % data,
+                'Fetched command from controller: %r' % data,
                 logger=self.agent.logger,
                 level=logging.INFO
             )
-            self.agent.maybe_add_command_to_queue(data)
+            command_dict = json.loads(data)
+            self.agent.maybe_add_command_to_queue(command_dict)
         else:
             maybe_log_message(
                 'No command fetched from controller.',
@@ -30,7 +44,6 @@ class ExecuteCommandTask(BaseTask):
     DEFAULT_TIMEOUT = 60
 
     def handle(self):
-        if self.supervisor:
-            self.supervisor.schedule(
-                self.agent.execute_command, timeout=self.DEFAULT_TIMEOUT
-            )
+        self.supervisor.schedule(
+            self.agent.execute_command, timeout=self.DEFAULT_TIMEOUT
+        )

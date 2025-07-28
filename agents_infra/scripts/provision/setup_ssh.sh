24b5d88f2a347f4916574731e4354c29e616749a
devdsys <dimakyrytschuk@gmail.com>
2025-07-28 23:43:14 +0300

    refactor: switch SSH password provisioning from env vars to Docker secrets

--- agents_infra/scripts/provision/setup_ssh.sh
+++ agents_infra/scripts/provision/setup_ssh.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -e
 
-SSH_PASSWORD="$1"
+mkdir -p /var/run/sshd
 
-mkdir /var/run/sshd
-echo "root:${SSH_PASSWORD}" | chpasswd
-sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+# Set root password from secret
+PW=$(cat /run/secrets/ssh_password)
+echo "root:$PW" | chpasswd
 
+# Allow SSH login for root
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
+# Set default directory on login for root
 echo "cd /app" >> /root/.bashrc
+
+exec /sbin/init

3a1a368f4a0f2463e9c50ee9b97fe87ef7b8fe1f
devdsys <dimakyrytschuk@gmail.com>
2025-07-30 16:09:58 +0300

    refactor: switch SSH access from root to dedicated user

--- agents_infra/scripts/provision/setup_ssh.sh
+++ agents_infra/scripts/provision/setup_ssh.sh
@@ -3,15 +3,22 @@ set -e
 
 mkdir -p /var/run/sshd
 
-# Set root password from secret
+# Ensure a dedicated non-root user 'user' exists for SSH access
+if ! id -u user >/dev/null 2>&1; then
+    useradd -m -s /bin/bash user
+fi
+
+# Set the SSH password for 'user' from the Docker secret
 PW=$(cat /run/secrets/ssh_password)
-echo "root:$PW" | chpasswd
+echo "user:$PW" | chpasswd
 
-# Allow SSH login for root
-sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+# Harden SSH configuration: disable root login, enable password authentication
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
 sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i '/^\s*account\s\+required\s\+pam_nologin\.so/s/^/#/' /etc/pam.d/sshd
 
-# Set default directory on login for root
-echo "cd /app" >> /root/.bashrc
+# Set the default working directory for 'user' upon SSH login
+echo "cd /app" >> /home/user/.bashrc
+chown user:user /home/user/.bashrc
 
 exec /sbin/init
